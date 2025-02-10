d3e50523a9494b78ec7708c17d77feac03682f17
Thiago Paiva <thiago.paiva@urbana-pe.com.br>
2025-02-10 19:58:27 -0300

    impÂlementando loggin #F043

--- src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -6,21 +6,25 @@ import com.ead.authuser.enums.UserType;
 import com.ead.authuser.models.UserModel;
 import com.ead.authuser.services.UserService;
 import com.fasterxml.jackson.annotation.JsonView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+
 
 @RequestMapping("/auth")
 @RestController
 public class AuthenticationController {
+
+
+    Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
 
     @Autowired
     UserService userService;
@@ -45,5 +49,15 @@ public class AuthenticationController {
         userModel.setLastUpdateDate(LocalDateTime.now(ZoneId.of("UTC")));
         userService.save(userModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(userModel);
+    }
+
+
+
+    @GetMapping()
+    public String index(){
+        logger.trace("Testando mizinga");
+        logger.debug("DEBUG MIZINGA");
+        logger.info("INFO MIZINGA");
+        return "Logging spring boot";
     }
 }
